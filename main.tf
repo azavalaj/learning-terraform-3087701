c1e6052b8d46b9d8f444a379f2257a4cc3a09c65
azavalaj <azavalaj@yahoo.com>
2026-06-10 23:48:45 -0400

    supress name filter

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
