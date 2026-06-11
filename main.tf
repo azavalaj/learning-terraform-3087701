27fc9dca4d427ea3553a1b0671970937971eef27
azavalaj <azavalaj@yahoo.com>
2026-06-11 16:37:48 -0400

    blog & t3.micro

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["345002264488"] # 
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
